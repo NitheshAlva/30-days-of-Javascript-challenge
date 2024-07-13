0153281b06264b2ef8d378af2e48474c09189f97
NitheshAlva <alvanitheshcod05@gmail.com>
2024-07-13 19:40:04 +0530

    Variables Day 1 of 30 days of js

---
+++ day1.js
@@ -0,0 +1,30 @@
+//activity-1
+var num=999
+console.log(num)
+
+let str="abcd"
+console.log(str)
+
+const flag=true
+console.log(flag)
+
+let myObj={
+    firstName:"Nithesh",
+    lastName:"Alva",
+    age:20
+}
+
+const arr=['hdj',373,myObj,num]
+
+console.log(typeof num)
+console.log(typeof str)
+console.log(typeof flag)
+console.log(typeof myObj)
+console.log(typeof arr)
+
+
+//re-assigning variables
+num=900
+
+// flag=false cannot be overidden
+
