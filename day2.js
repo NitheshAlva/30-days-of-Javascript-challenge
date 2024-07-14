5829a49259d4df4ed3b1db4700b5783c0acbd531
NitheshAlva <alvanitheshcod05@gmail.com>
2024-07-14 08:56:34 +0530

    Operators Day 2 of 30 days of JS

---
+++ day2.js
@@ -0,0 +1,21 @@
+// Arithmetic operators
+console.log(10+20)
+console.log(10-20)
+console.log(10*20)
+console.log(10/20)
+console.log(10%20)
+
+//Assignment operator
+x=100
+x+=50
+console.log(x)
+x-=100
+console.log(x)
+
+//comparison and logical operators 
+console.log(10>20||10<20)
+console.log(10<=10&&10<=9)
+console.log(11==11^1)
+
+//ternary operator
+console.log((x>=0)?"x is positive":"x is negative")
